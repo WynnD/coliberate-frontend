d8d73fbeab401683329324f06482d99b35341ea0
Joshua Castor <jcasto3@uic.edu>
2018-02-12 08:47:34 -0600

    Fix indentation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,13 @@
 <template>
-    <div id="home">
-        <div id="button-wrapper">
-            <a href="javascript:(function()%7Bjavascript:var%20s%3Ddocument.createElement(%27script%27)%3Bs.setAttribute(%27src%27,%27https://nthitz.github.io/turndownforwhatjs/tdfw.js%27)%3Bdocument.body.appendChild(s)%3B%7D)()%3B">Make this webpage fun again!</a>
-        </div>
+  <div id="home">
+    <div id="button-wrapper">
+      <a href="javascript:(function()%7Bjavascript:var%20s%3Ddocument.createElement(%27script%27)%3Bs.setAttribute(%27src%27,%27https://nthitz.github.io/turndownforwhatjs/tdfw.js%27)%3Bdocument.body.appendChild(s)%3B%7D)()%3B">Make this webpage fun again!</a>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Home'
+  name: 'Home'
 }
 </script>
