b5f3add18fa4584ce9b8181b829255358300390a
Joshua Castor <jcasto3@uic.edu>
2018-02-19 14:20:57 -0600

    Made sidebar close on link click

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,24 +3,52 @@
     class="ui sidebar inverted vertical menu"
     id="sidebar">
     <router-link
+      @click.native="hideSidebar"
       class="item"
       to="/">
       Overview
     </router-link>
     <router-link
+      @click.native="hideSidebar"
       class="item"
       to="/projects">
       Projects
     </router-link>
     <router-link
+      @click.native="hideSidebar"
       class="item"
       to="/members">
       Members
     </router-link>
     <router-link
+      @click.native="hideSidebar"
       class="item"
       to="/settings">
       Settings
     </router-link>
   </div>
 </template>
+
+<script>
+/* global $ */
+export default {
+  data () {
+    return {
+      sidebar: null
+    }
+  },
+  mounted () {
+    this.sidebar = $(this.$el)
+    this.sidebar.sidebar({
+      transition: 'overlay'
+    })
+  },
+  methods: {
+    hideSidebar () {
+      // eslint-disable-next-line
+      console.log("Hiding sidebar")
+      this.sidebar.sidebar('hide')
+    }
+  }
+}
+</script>
