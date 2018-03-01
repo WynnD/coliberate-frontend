b4fa383462a20790f4b4280e34d1195c593a59ff
Wynn Drahorad <wynn.drahorad@gmail.com>
2018-03-01 10:01:09 -0600

    small change to design of homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,24 +2,22 @@
   <div
     id="home"
     class="ui container">
-    <div class="ui segment">
-      <h2 class="ui header">Welcome to Coliberate!</h2>
-      <div class="ui segments">
-        <div class="ui black inverted segment">Project Information</div>
-        <div class="ui segment">
-          <div class="ui header">What is Coliberate?</div>
-          <p>Coliberate is a cloud-based Computer-Aided Software Engineering (CASE) tool intended to help teams manage their projects.</p>
-          <p>This project is developed by Group 1 of CS 442 Spring 2018 at UIC.</p>
-        </div>
-        <div class="ui segment">
-          <div class="ui header">Group 1</div>
-          <ul>
-            <li>Joseph Borowicz</li>
-            <li>Joshua Castor</li>
-            <li>Wynn Drahorad</li>
-            <li>Kaeyan Jones</li>
-          </ul>
-        </div>
+    <h2 class="ui header">Welcome to Coliberate!</h2>
+    <div class="ui segments">
+      <div class="ui black inverted segment">Project Information</div>
+      <div class="ui segment">
+        <div class="ui header">What is Coliberate?</div>
+        <p>Coliberate is a cloud-based Computer-Aided Software Engineering (CASE) tool intended to help teams manage their projects.</p>
+        <p>This project is developed by Group 1 of CS 442 Spring 2018 at UIC.</p>
+      </div>
+      <div class="ui segment">
+        <div class="ui header">Group 1</div>
+        <ul>
+          <li>Joseph Borowicz</li>
+          <li>Joshua Castor</li>
+          <li>Wynn Drahorad</li>
+          <li>Kaeyan Jones</li>
+        </ul>
       </div>
     </div>
   </div>
