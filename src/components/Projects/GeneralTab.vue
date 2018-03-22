a2f78d7c5c611a4dec0f5972cafbe17fbd62075c
Joshua Castor <jcasto3@uic.edu>
2018-03-22 13:07:06 -0500

    Added initial files for landing page, details, and general tab for a single project

---
+++ src/components/Projects/GeneralTab.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    General: {{ project }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {
+      required: true,
+      type: Object
+    }
+  }
+}
+</script>
