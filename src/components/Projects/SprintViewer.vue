7a2a67b539077cf0da315fd3992f24e653202387
Joshua Castor <jcasto3@uic.edu>
2018-04-08 18:39:59 -0500

    Show sprint names instead of Ids in sprint viewer

--- src/components/Projects/SprintViewer.vue
+++ src/components/Projects/SprintViewer.vue
@@ -11,7 +11,7 @@
           v-for="sprintId in release.sprints"
           :key="sprintId"
           :value="sprintId">
-          {{ sprintId }}
+          {{ sprints[sprintId].name || sprintId }}
         </option>
       </select>
       <span
