a6babfa7dac79eaccf17104987d374413a36daa8
Joshua Castor <jcasto3@uic.edu>
2018-04-08 11:57:13 -0500

    Created a generic generate unique ID function for use in creating IDs for project objects

--- src/store/index.js
+++ src/store/index.js
@@ -211,6 +211,25 @@ export const state = {
   developmentMode: false
 }
 
+const generateUniqueId = (parentObject = {}, prefix = 'id-', numDigits = 4) => {
+  const createID = (prefix, number) => `${prefix}${number.toString().padStart(numDigits, '0')}`
+  const max = +(`1${'0'.padStart(numDigits, 0)}`)
+  let numberId = Math.floor(Math.random() * max)
+  let numIterations = 0
+  let currentId = createID(prefix, numberId)
+  while (parentObject[currentId] && numIterations < max) {
+    numberId = Math.floor(Math.random() * max)
+    currentId = createID(prefix, numberId)
+    numIterations++
+  }
+
+  if (!parentObject[currentId]) {
+    return currentId
+  } else {
+    return generateUniqueId(parentObject, `${currentId}-`, numDigits)
+  }
+}
+
 export const getters = {
   projectIds: state => Object.keys(state.projects),
   newProjectId: state => Math.floor(Math.random() * 10000).toString().padStart(4, '0'),
@@ -218,7 +237,8 @@ export const getters = {
   isLoggedIn: state => !!state.accountData,
   isDevelopmentMode: state => !!state.developmentMode,
   currentUser: state => state.accountData,
-  memberById: state => id => state.members[id]
+  memberById: state => id => state.members[id],
+  generateUniqueId: state => generateUniqueId
 }
 
 export const mutations = {
